a9a67cdc3f11437cc8a3e547fdb8dc30fd8a0ad2
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-04-16 09:36:21 -0700

    Update scripts.js

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,6 +1,8 @@
 
 const randUserUrl = 'https://randomuser.me/api/?results=12&nat=us';
-const gallery = document.getElementById('gallery');
+const gallery = document.querySelector('#gallery');
+const title = document.querySelector('title');
+    console.log(title);
     console.log(gallery);
 let profiles = [];
 let data;
@@ -13,22 +15,28 @@ async function registrar(url) {
     const peopleResponse = await fetch(url)
         .then(response => response.json())
         .then(data => {
-            data.results.map(person => {
-            person =
-                `<div class="card">
-                    <div class="card-img-container">
-                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-                    </div>
-                    <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">first last</h3>
-                        <p class="card-text">email</p>
-                        <p class="card-text cap">city, state</p>
-                    </div>
-                </div>`
-
-            gallery.insertAdjacentHTML('beforeend', person) 
-            })
+            profiles = data.results;
+            console.log(profiles);
+            generateProfiles(profiles);
         })
 };
+
+function generateProfiles(arr) {
+    arr.map(person => {
+        person =
+            `<div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${person.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                    <p class="card-text">${person.email}</p>
+                    <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+                </div>
+            </div>`
+
+        title.insertAdjacentHTML('beforeend', person) 
+        })
+}
 
 registrar(randUserUrl)
