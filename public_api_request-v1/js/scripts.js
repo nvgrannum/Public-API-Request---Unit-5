6f094ec7c1302eba70e1f775e66b37e54f9cdd1f
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-04-16 09:03:38 -0700

    Update scripts.js

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,37 +1,34 @@
 
 const randUserUrl = 'https://randomuser.me/api/?results=12&nat=us';
-const gallery = document.querySelector('title');
+const gallery = document.getElementById('gallery');
     console.log(gallery);
 let profiles = [];
 let data;
 
 /*
-Makes a new promise and goes to the url 12 times. Each time it goes to the URL, 
-it 'gets' the JSON and stores it in the array 'profiles'. 
+
 */
 
 async function registrar(url) {
-    const response = await fetch(url);
-    profiles = await response.json()
-        .then(profile => console.log(profile));  
-    return profiles;
-}
+    const peopleResponse = await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.results.map(person => {
+            person =
+                `<div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">first last</h3>
+                        <p class="card-text">email</p>
+                        <p class="card-text cap">city, state</p>
+                    </div>
+                </div>`
 
-let test = registrar(randUserUrl)
+            gallery.insertAdjacentHTML('beforeend', person) 
+            })
+        })
+};
 
-
-test.forEach(person => {
-   person =
-    `<div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">first last</h3>
-            <p class="card-text">email</p>
-            <p class="card-text cap">city, state</p>
-        </div>
-    </div>`
-
-gallery.insertAdjacentHTML('beforeend', person) 
-});
+registrar(randUserUrl)
